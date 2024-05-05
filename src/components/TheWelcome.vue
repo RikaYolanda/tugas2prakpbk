e9d4f29159d5939c43d9ca8ec01c39e146b2dce5
RikaYolanda <168209002+RikaYolanda@users.noreply.github.com>
2024-05-05 21:28:15 +0700

    Add files via upload

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <label for="inputText">Input Text:</label>
+      <input type="text" id="inputText" v-model="inputText" @input="updateText">
+    </div>
+    <div :style="{ marginTop: '20px', fontSize: '18px' }">
+      <p v-text="displayText"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: '',
+      displayText: ''
+    };
+  },
+  methods: {
+    updateText() {
+      this.displayText = this.inputText;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
